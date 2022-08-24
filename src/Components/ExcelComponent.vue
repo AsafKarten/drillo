4e88670c0940da9123ad239b62ae4b486b7e79cf
Your Name <asafkarten@gmail.com>
2022-08-24 21:58:22 +0300

    sucssesfully converted itm to lat,lon

--- src/Components/ExcelComponent.vue
+++ src/Components/ExcelComponent.vue
@@ -13,6 +13,7 @@
 
 <script>
 import * as XLSX from "xlsx";
+import proj4 from 'proj4'
 export default {
   name: "ExcelComponent",
   data: function () {
@@ -41,12 +42,16 @@ export default {
         var arraylist = XLSX.utils.sheet_to_json(worksheet, { raw: true });
         this.filelist = [];
         //console.log(this.filelist);
+              proj4.defs('EPSG:2039', '+proj=tmerc +lat_0=31.73439361111111 +lon_0=35.20451694444445 +k=1.0000067 +x_0=219529.584 +y_0=626907.39 +ellps=GRS80 +towgs84=-48,55,52,0,0,0,0 +units=m +no_defs');
+        // WGS84
+              proj4.defs('EPSG:4326', '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees');
 
         var pits= []
         for(i = 0 ; i < arraylist.length ; i++){
             let p = arraylist[i].p;
             let itm = {x : arraylist[i].x , y : arraylist[i].y }
-            pits[i]= {p:p , itm:itm , coordinates: {}}
+            const [long, lat] = proj4(proj4('EPSG:2039'), proj4('EPSG:4326'), [itm.x, itm.y]);
+            pits[i]= {p:p , itm:itm , coordinates: {long:long,lat:lat}}
         }
         console.log(pits);
       };
