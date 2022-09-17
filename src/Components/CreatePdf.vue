fd149d39d8addfde756f52d850c19d81a4b0d588
noyil <noycom@gmail.com>
2022-09-17 21:04:49 +0300

    Added signature to CreatePDF (still in development)

--- src/Components/CreatePdf.vue
+++ src/Components/CreatePdf.vue
@@ -2,6 +2,17 @@
   <button @click="generatePDF">צור דוח</button>
   <button @click="getReportDiv">דבג דוח</button>
 
+
+<div>
+  <Vue3Signature  ref="signature1" :sigOption="state.option" :w="'1280px'" :h="'400px'"
+                 :disabled="state.disabled" class="example"></Vue3Signature>
+  <button @click="save('image/jpeg')">Save</button>
+  <button @click="clear">Clear</button>
+  <button @click="undo">Undo</button>
+  <button @click="addWaterMark">addWaterMark</button>
+  <button @click="handleDisabled">disabled</button>
+</div>
+
   <div ref="reportDiv" style="display:none;">
 
         <table style="direction:ltr; border-spacing: 10px; border-collapse: separate;">
@@ -32,7 +43,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent, onMounted, ref, reactive } from 'vue';
 import { useRouter } from 'vue-router';
 import {useAppState} from '../realm-state';
 
@@ -40,11 +51,12 @@ import pdfMake from 'pdfmake';
 import htmlToPdfmake from 'html-to-pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 
+import Vue3Signature from "vue3-signature"
 
 /* eslint-disable */
 export default defineComponent({
   name: 'UploadFile',
-  components: {},
+  components: {Vue3Signature},
   props:{report:Object},
   setup(props){
     const router = useRouter();
@@ -71,13 +83,54 @@ export default defineComponent({
       //add code or delete
     });
 
+
+
+
+
+
+
+    //SIGNATURE PAD
+
+    const state = reactive({
+      count: 0,
+      option: {
+        penColor: "rgb(0, 0, 0)",
+        backgroundColor: "rgb(255,255,255)"
+      },
+      disabled: false
+    })
+
+    const signature1 = ref(Vue3Signature)
+    const save = (t:String) => { console.log(signature1.value.save(t)) }
+    const clear = () => { signature1.value.clear() }
+    const undo = () => { signature1.value.undo(); }
+    const handleDisabled = () => { state.disabled = !state.disabled }
+    const addWaterMark = () => { signature1.value.addWaterMark({
+        text: "mark text",          // watermark text, > default ''
+        font: "20px Arial",         // mark font, > default '20px sans-serif'
+        style: 'all',               // fillText and strokeText,  'all'/'stroke'/'fill', > default 'fill
+        fillStyle: "red",           // fillcolor, > default '#333'
+        strokeStyle: "blue",	       // strokecolor, > default '#333'
+        x: 100,                     // fill positionX, > default 20
+        y: 200,                     // fill positionY, > default 20
+        sx: 100,                    // stroke positionX, > default 40
+        sy: 200                     // stroke positionY, > default 40
+    }); }
+
+
+
     return {
       //methods
       generatePDF,
       getReportDiv,
 
       //properties
-      reportDiv
+      reportDiv,
+
+
+      //SIGNATURE PAD
+      signature1, state,
+      save, clear, undo, handleDisabled, addWaterMark
     }
   }
 });
