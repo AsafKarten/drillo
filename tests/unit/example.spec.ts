c5216ba75a94d5781501686958e169584b58f7d8
Your Name <asafkarten@gmail.com>
2022-12-18 00:29:56 +0200

    changed weapper to app

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,11 @@
 import { mount } from '@vue/test-utils'
 import HomePage from '@/views/HomePage.vue'
 import Login from '@/views/Login.vue'
+import App from '@/App.vue'
 
-describe('Login.vue', () => {
+describe('App.vue', () => {
   it('renders home vue', () => {
-    const wrapper = mount(Login)
+    const wrapper = mount(App)
     expect(wrapper.text()).toMatch('Ready to create an app?')
   })
 })
