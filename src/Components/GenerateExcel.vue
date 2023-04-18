8097ade41d71752f7d8b7fe82274504c72653f1d
Your Name <asafkarten@gmail.com>
2023-04-18 21:48:36 +0300

    continued with payment report (tables are hard!)

--- src/Components/GenerateExcel.vue
+++ src/Components/GenerateExcel.vue
@@ -53,40 +53,82 @@
         <!--payment riport table-->
         <table id="payment-table-id" v-show="paymentReport" >
        
-          <thead>
-            <tr>
-               <th colspan="10" style="text-align: center;" >{{'חשבון לחודש'+' '+month + ' ' + 'עבור קידוח כלונסאות' + ' ' + project?.name + ' ' + project?.address}}</th>
+          <thead  >
+            <tr >
+               <th colspan="14" style="text-align: center;" >{{'חשבון לחודש'+' '+month + ' ' + 'עבור קידוח כלונסאות' + ' ' + project?.name + ' ' + project?.address}}</th>
             </tr>
             <tr>
             <th colspan="1">מס' רץ</th>
-            <th style="text-align: center;" colspan="7">תאור</th>
-            <th colspan="1"></th>
-            <th colspan="1"></th>
+            <th style="text-align: center;" colspan="11">תאור</th>
+            <th colspan="1" rowspan="2">מחיר יחידה</th>
+            <th colspan="1" rowspan="2" >סה"כ</th>
             </tr>
             
-         </thead>
+         
           
           <tr>
             <th>#</th>
-            <th>תאריך</th>
-            <th>יום</th>
-            <th>מס' כלונס</th>
+            <th colspan="2">תאריך</th>
+            <th colspan="2">יום</th>
+            <th colspan="2">מס' כלונס</th>
             <th>קוטר</th>
             <th>עומק</th>
             <th>כמות</th>
             <th>מטר אורך</th>
-            <th colspan="1">מחיר יחידה</th>
-            <th colspan="1">סה"כ</th>
+            
           </tr>
-          <tr :key="pit?._id" v-for="pit in pits">
-            <td>{{pit.listName}}</td>
-            <td>{{pit.p}}</td>
-            <td>{{pit.depth}}</td>
-            <td>{{pit.diameter}}</td>
-            <td>{{pit.concreteVolume}}</td>
-            <td>{{(pit.depth * price)}}</td>
-            <td>{{pit.finishDate.getDate() + '/' + (pit.finishDate.getMonth() * 1 + 1) + '/' + pit.finishDate.getFullYear()}}</td>
+        </thead>
+     
+          <tr :key="day?.number" v-for="day in daysOfWork">
+            
+            <td>{{day.number}}</td>
+            <td>{{day.currentDate.getDate() + '/' + (day.currentDate.getMonth() * 1 + 1) + '/' + day.currentDate.getFullYear()}}</td>
+            <!-- <PaymentTableRows :row="day.rows" :currentDay="day.day"/> -->
+            <td colspan="2">
+              <tr colspan="2" :key="row?.listName" v-for="row in day.rows">
+                <td colspan="2">{{day.day}}</td>
+                
+              </tr>
+           </td>
+           <td colspan="2">
+            <tr colspan="2" :key="row?.listName" v-for="row in day.rows">
+              <td>{{row.listName + ": "}}{{row.columnsNumbers}}</td>
+            </tr>
+         </td>
+         <td colspan="2">
+          <tr colspan="2" :key="row?.listName" v-for="row in day.rows">
+            <td>{{row.diameter}}</td>
           </tr>
+       </td>
+       <td colspan="1">
+        <tr colspan="1" :key="row?.listName" v-for="row in day.rows">
+          <td>{{row.depth}}</td>
+        </tr>
+     </td>
+     <td colspan="1">
+      <tr colspan="1" :key="row?.listName" v-for="row in day.rows">
+        <td>{{row.amount}}</td>
+      </tr>
+   </td>
+   <td colspan="2">
+    <tr colspan="2" :key="row?.listName" v-for="row in day.rows">
+      <td>{{row.totalDepth}}</td>
+    </tr>
+ </td>
+ <td colspan="1">
+  <tr colspan="1" :key="row?.listName" v-for="row in day.rows">
+    <td>{{price}}</td>
+  </tr>
+</td>
+ <td colspan="2">
+  <tr colspan="2" :key="row?.listName" v-for="row in day.rows">
+    <td>{{(row.totalDepth*price)}}</td>
+  </tr>
+</td>
+           
+              
+            </tr>
+           
         </table>
 
 
@@ -105,6 +147,7 @@ import OfficeAppHeader from '../Components/OfficeAppHeader.vue'
 
 //imports 
 import * as XLSX from "xlsx";
+import PaymentTableRows from './PaymentTableRows.vue';
 
 
 
@@ -126,8 +169,8 @@ export default defineComponent({
     //IonTitle,
     //IonButtons,
     OfficeAppHeader,
-
-  },
+    //PaymentTableRows
+},
   setup() {
     const router = useRouter();
     const route = useRoute()
@@ -144,6 +187,8 @@ export default defineComponent({
     const paymentReport = ref(false)
     const isOpenMonth = ref(false)
     const month = ref()
+
+    const daysOfWork = ref<any>()
     
     onIonViewWillEnter(async()=>{
       allPits.value = await getProjectPits(project_id.value)
@@ -173,9 +218,103 @@ export default defineComponent({
       sortPits()
       let dateVar = new Date(startDate.value)
       month.value ="" + (dateVar.getUTCMonth()*1+1) + "/" + dateVar.getFullYear()
+      makePaymetRows()
       regularReport.value = false
       paymentReport.value = true
       
+    }
+
+    const makePaymetRows = ()=>{
+      let daysOfWeek = ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת']
+      let day = ''
+      let rows = [{listName:'', depth:0, diameter:0, columnsNumbers:'', amount:0, totalDepth:0 }]
+      let numOfDays = []
+      let currentNumber = 1
+      let numOfColumns = 1
+      let currentDate = null
+      for (let index = 0; index < pits.value.length; index++) {
+        let date = pits.value[index].finishDate.setHours(0, 0, 0, 0)
+        if(index === 0){
+          currentDate =pits.value[index].finishDate.setHours(0, 0, 0, 0)
+          day = daysOfWeek[pits.value[index].finishDate.getDay()]
+          numOfDays.push({number : currentNumber , currentDate : new Date(currentDate)  , day: day , numOfColumns : numOfColumns, rows:rows })
+          
+        }
+        else if(date === currentDate){
+          numOfColumns+=1
+          numOfDays[numOfDays.length-1].numOfColumns = numOfColumns
+
+        }
+        else{
+          numOfDays[numOfDays.length-1].rows =sortPaymentPits(currentDate)
+          numOfColumns = 1
+          currentNumber++
+          day = daysOfWeek[pits.value[index].finishDate.getDay()]
+          currentDate =pits.value[index].finishDate.setHours(0, 0, 0, 0)
+          numOfDays.push({number : currentNumber , currentDate :new Date(currentDate) , day: day , numOfColumns : numOfColumns,  rows:rows  })
+        }
+      }
+      numOfDays[numOfDays.length-1].rows =sortPaymentPits(currentDate)
+      console.log(numOfDays);
+      daysOfWork.value = numOfDays
+      
+    }
+
+    const sortPaymentPits = (finishDate:number)=>{
+      let rows = []
+      let listName=''
+      let depth = 0
+      let diameter = 0
+      let columnsNumbers = ''
+      let amount = 0
+      let totalDepth = 0
+      let tempDate = new Date(finishDate)
+      let sortedPits = pits.value.filter((pit: { finishDate: Date; status: string; })=> pit.finishDate?.setHours(0, 0, 0, 0).valueOf() === tempDate.setHours(0, 0, 0, 0).valueOf())
+
+      for (let index = 0; index < sortedPits.length; index++) {
+        let pit = sortedPits[index]
+
+        if(index === 0){
+          //the first pit is inserted twice ::fix
+          listName = pit.listName
+          depth = pit.depth
+          diameter = pit.diameter
+          columnsNumbers = '' + pit.p
+          amount = 1
+          totalDepth = pit.depth
+          rows.push({listName:listName, depth:depth, diameter:diameter, columnsNumbers:columnsNumbers, amount:amount, totalDepth:totalDepth })
+        }
+        else if (listName === pit.listName && depth === pit.depth && diameter === pit.diameter){
+          amount++
+          rows[rows.length-1].columnsNumbers = rows[rows.length-1].columnsNumbers + ", " + pit.p
+          rows[rows.length-1].amount= amount
+          rows[rows.length-1].totalDepth += pit.depth
+        }
+        else{
+          let row = rows.find((row)=>row.listName === pit.listName && row.depth === pit.depth && row.diameter === pit.diameter)
+          if(row !== undefined){
+            let rowIndex = rows.indexOf(row)
+            let tempAmount = rows[rowIndex].amount + 1
+            rows[rowIndex].columnsNumbers = rows[rows.length-1].columnsNumbers + ", " + pit.p
+            rows[rowIndex].amount= tempAmount
+            rows[rowIndex].totalDepth += pit.depth
+    
+          }
+          else{
+          listName = pit.listName
+          depth = pit.depth
+          diameter = pit.diameter
+          columnsNumbers = '' + pit.p
+          amount = 1
+          totalDepth = pit.depth
+          rows.push({listName:listName, depth:depth, diameter:diameter, columnsNumbers:columnsNumbers, amount:amount, totalDepth:totalDepth })
+          }
+
+         
+        }
+        
+      }
+      return rows
     }
 
     const sortPits = () => {
@@ -244,6 +383,8 @@ export default defineComponent({
       createXl,
       generateRegularReport,
       generatePaymentReport,
+      makePaymetRows,
+      sortPaymentPits,
       sortPits,
       getFullReport,
       openChooseMonth,
@@ -260,6 +401,8 @@ export default defineComponent({
       paymentReport,
       isOpenMonth,
       month,
+
+      daysOfWork,
     }
   },
 
@@ -274,9 +417,10 @@ table {
 }
 
 td, th {
-  border: 1px solid #dddddd;
+  border: 1px solid #000000;
   text-align: left;
   padding: 8px;
+  text-align: center;
 }
 
 tr:nth-child(even) {
---
+++ src/Components/PaymentTableRows.vue
@@ -0,0 +1,112 @@
+<template>
+
+
+          <!--payment report row-->
+     <table>
+            <tr :key="row?.listName" v-for="row in day">
+              <td colspan="2">{{workDay}}</td>
+              <td colspan="2">{{row.columnsNumbers}}</td>
+              <td colspan="2">{{row.diameter}}</td>
+              
+            </tr>
+    </table>
+ 
+  </template>
+    
+  <script lang="ts">
+  import { onIonViewWillEnter,onIonViewDidLeave, IonContent, IonHeader, IonPage, IonToolbar, IonButton, IonButtons, IonModal, IonTitle, IonInput, IonLabel, IonItem, IonLoading} from '@ionic/vue';
+  import { defineComponent, onMounted, ref, render } from 'vue';
+  import { useRouter, useRoute } from 'vue-router';
+  import { useAppState } from '../realm-state';
+
+  
+  //imports 
+
+  
+  
+  
+  
+  
+  
+  export default defineComponent({
+    name: 'GenerateExcel',
+    components: {
+      //IonPage,
+      //IonContent,
+      //IonItem,
+      //IonLabel,
+      //IonInput,
+      //IonButton,
+  
+ 
+  
+    },
+    props:{row:Object, currentDay:String},
+    setup(props) {
+      const router = useRouter();
+      const route = useRoute()
+      const { user,getProjectPits, getProjectByID} = useAppState(); 
+      const currentUser = ref<any>(user);
+      const day = ref<any>(props.row)
+      const workDay = ref<any>(props.currentDay)
+     
+      onMounted(async()=>{
+     console.log(day.value);
+     
+        
+        
+      });
+  
+  
+
+  
+      return {
+        day,
+        workDay
+      }
+    },
+  
+  });
+  </script>
+    
+  <style scoped>
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+  
+  td, th {
+    border: 1px solid #000000;
+    text-align: left;
+    padding: 8px;
+    text-align: center;
+  }
+  
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+  
+  ion-button {
+    --background: #4DBA87;
+    --background-hover: #9ce0be;
+    --background-activated: #88f4be;
+    --background-focused: #88f4be;
+  
+  
+    --color: #fff;
+  
+    --border-radius: 10px;
+    --border-color: #4DBA87;
+    --border-style: solid;
+    --border-width: 2px;
+  
+    --box-shadow: 0 2px 6px 0 rgb(0, 0, 0, 0.25);
+  
+    --ripple-color: deeppink;
+  
+    --padding-top: 10px;
+    --padding-bottom: 10px;
+  
+  }
+  </style>
